fa942dbbcdc45f5bb49ce74967b45ee746484107
diogeneskelsen@gmail.com <diogeneskelsen@gmail.com>
2021-10-17 14:23:28 +0100

    Updating docs and features
    + Adding endpoint to list cards by status
    + Adding new card attribute: ativo
    + Adding random card number support
    + Card attribute ativo is updated to false after card redeem
    + Documentation update

--- backend/domain/cartao.py
+++ backend/domain/cartao.py
@@ -1,3 +1,5 @@
+import string
+import random
 from datetime import datetime
 from flask import jsonify, make_response, abort
 from dotenv import dotenv_values
@@ -21,8 +23,29 @@ def get_dict_from_mongodb(search_key):
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
+def card_number_generator(size=16, chars=string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 def read_all():
-    CARDS = get_dict_from_mongodb("email_proprietario")
+    CARDS = get_dict_from_mongodb("timestamp")
+    dict_cartoes = [CARDS[key] for key in sorted(CARDS.keys())]
+    cartoes = jsonify(dict_cartoes)
+    qtd = len(dict_cartoes)
+    content_range = "cartoes 0-"+str(qtd)+"/"+str(qtd)
+    # Configura headers
+    cartoes.headers['Access-Control-Allow-Origin'] = '*'
+    cartoes.headers['Access-Control-Expose-Headers'] = 'Content-Range'
+    cartoes.headers['Content-Range'] = content_range
+    return cartoes
+
+
+def read_all_by_status(ativo: bool):
+    itens_db = db.cartoes.find({"ativo": ativo})
+    CARDS = {}
+    for i in itens_db:
+            i.pop('_id') # retira id: criado automaticamente pelo mongodb
+            item = dict(i)
+            CARDS[item["numero_cartao"]] = (i)
     dict_cartoes = [CARDS[key] for key in sorted(CARDS.keys())]
     cartoes = jsonify(dict_cartoes)
     qtd = len(dict_cartoes)
@@ -56,7 +79,8 @@ def create(card):
             "email_proprietario": email_proprietario,
             "email_beneficiario": email_beneficiario,
             "saldo": saldo,
-            "numero_cartao": "1111222233334444",
+            "ativo": True,
+            "numero_cartao": card_number_generator(),
             "prazo": prazo,
             "timestamp": get_timestamp(),
         }
@@ -78,9 +102,10 @@ def update(numero_cartao, transacao):
     if numero_cartao in CARDS:
         card = CARDS.get(numero_cartao)
         novo_saldo = float(card["saldo"]) - float(transacao.get("valor"))
-        if novo_saldo >= 0:
+        if novo_saldo >= 0 and card["ativo"] is not False:
             update = { "$set": {
                     "saldo": novo_saldo,
+                    "ativo": False,
                     "timestamp": get_timestamp(), } 
                 }
             db.cartoes.update_one(query, update)
